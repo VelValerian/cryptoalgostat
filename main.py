54cb3dbbe9a8f956865b3f136377d7798f5fdb99
Valerian <@VelValerian>
2023-08-15 16:18:41 +0300

    Add keyboard exchange

--- main.py
+++ main.py
@@ -15,16 +15,23 @@ help_command = """
 /help - command list
 /exchanges - cryptocurrency exchanges list"""
 
-exchanges = """
-Binance
-Huobi
-OKX"""
+# exchanges = """
+# Binance
+# Huobi
+# OKX"""
 
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 butt_1 = KeyboardButton('/help')
 butt_2 = KeyboardButton('/exchanges')
 
 kb.add(butt_1,).add(butt_2,)
+
+kb_exchanges = ReplyKeyboardMarkup(resize_keyboard=True)
+butt_exchanges_1 = KeyboardButton('/Binance')
+butt_exchanges_2 = KeyboardButton('/Huobi')
+butt_exchanges_3 = KeyboardButton('/OKX')
+
+kb_exchanges.add(butt_exchanges_1,).add(butt_exchanges_2, butt_exchanges_3)
 
 async def on_startup(_):
     print('Bot is up! \nReady to use')
@@ -37,14 +44,16 @@ async def start_cmd(message: types.Message):
                            reply_markup=kb)
     await message.delete()
     user_id = message.from_user.id
+    print(user_id)
 @dp.message_handler(commands='help')
 async def help_cmd(message: types.Message):
     await bot.send_message(chat_id=message.from_user.id, text=help_command, reply_markup=kb)
     await message.delete()
 
 @dp.message_handler(commands='exchanges')
-async def country_cmd(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id, text=exchanges, reply_markup=kb)
+async def exchanges_cmd(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id, text=md.text('<b>Select Exchanges</b>'),
+                           reply_markup=kb_exchanges)
     await message.delete()
 
 if __name__ == '__main__':
